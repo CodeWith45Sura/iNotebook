de74fc77f5f3cc382bf514588a71e8a1041f984f
CodeWith45Sura <152671939+CodeWith45Sura@users.noreply.github.com>
2024-05-17 07:55:26 +0530

    update database name

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-const mongoURI="mongodb+srv://sureshkcp77:<Suresh@78910>@inotebook.ei6qjvh.mongodb.net/test?retryWrites=true&w=majority&appName=iNotebook";
+const mongoURI="mongodb+srv://sureshkcp77:<Suresh@78910>@inotebook.ei6qjvh.mongodb.net/iNotebook?retryWrites=true&w=majority&appName=iNotebook";
 
 async function connectToMongo(){
    await mongoose.connect(mongoURI).then(()=>{
