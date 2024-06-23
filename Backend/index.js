ffe3a12927cf2d22d96dfc8955fdbc292dd5a8e3
CodeWith45Sura <152671939+CodeWith45Sura@users.noreply.github.com>
2024-06-23 17:05:06 +0530

    Update index.js cors headers

--- Backend/index.js
+++ Backend/index.js
@@ -8,9 +8,12 @@ const port = process.env.PORT || 5000 ;
 app.use(cors(
   {
    // https://i-notebook-frontend-pearl.vercel.app
-      origin:"*" ,
-      methods: ["POST" , "GET" ,"PUT" ],
-      credentials:true
+    //  origin:"*" ,
+     // methods: ["POST" , "GET" ,"PUT" ],
+     // credentials:true
+    res.setHeader( "Access-Control-Allow-Origin","*");
+     res.setHeader( "Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+ res.setHeader( "Access-Control-Allow-Methods","GET, POST, DELETE, PATCH, OPTIONS");
   }
 ))
 app.options('/*' , (_,res)=>{
